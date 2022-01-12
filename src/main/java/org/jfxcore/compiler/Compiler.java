26a4b297d2bd2122dbb266f7a6be81f7b2a9015f
mstr2 <43553916+mstr2@users.noreply.github.com>
2022-01-12 23:53:54 +0100

    Add .fxmlx extension to compiler (#38)

--- src/main/java/org/jfxcore/compiler/Compiler.java
+++ src/main/java/org/jfxcore/compiler/Compiler.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, JFXcore. All rights reserved.
+// Copyright (c) 2022, JFXcore. All rights reserved.
 // Use of this source code is governed by the BSD-3-Clause license that can be found in the LICENSE file.
 
 package org.jfxcore.compiler;
@@ -45,7 +45,7 @@ import java.util.Set;
 
 public class Compiler extends AbstractCompiler {
 
-    private static final String[] EXTENSIONS = new String[] {".fxml"};
+    private static final String[] EXTENSIONS = new String[] {".fxml", ".fxmlx"};
 
     private enum Stage {
         PARSE,
