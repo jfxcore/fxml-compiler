42b6e9bc3799c45be3ad690db1f2966be359ef7e
mstr2 <43553916+mstr2@users.noreply.github.com>
2023-08-29 21:15:13 +0200

    Improved compiler error message (#84)

--- src/main/java/org/jfxcore/compiler/Compiler.java
+++ src/main/java/org/jfxcore/compiler/Compiler.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, JFXcore. All rights reserved.
+// Copyright (c) 2022, 2023, JFXcore. All rights reserved.
 // Use of this source code is governed by the BSD-3-Clause license that can be found in the LICENSE file.
 
 package org.jfxcore.compiler;
@@ -236,14 +236,13 @@ public class Compiler extends AbstractCompiler {
     }
 
     private ClassPool newClassPool(Set<File> classpath) {
-        ClassPool classPool = new ClassPool();
-        classPool.appendSystemPath();
+        ClassPool classPool = new ClassPool(true);
 
         for (File cp : classpath) {
             try {
                 classPool.appendClassPath(cp.getAbsolutePath());
             } catch (NotFoundException ex) {
-                throw new RuntimeException(ex.getMessage());
+                throw new RuntimeException("Search path dependency not found: " + ex.getMessage());
             }
         }
 
