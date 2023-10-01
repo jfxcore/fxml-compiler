5bc2ff371f120a2121992b48b6bb1c5b66adfaf5
mstr2 <43553916+mstr2@users.noreply.github.com>
2023-10-01 14:59:04 +0200

    Revert logging changes (#107)

--- src/test/java/org/jfxcore/compiler/TestCompilationContext.java
+++ src/test/java/org/jfxcore/compiler/TestCompilationContext.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, 2023, JFXcore. All rights reserved.
+// Copyright (c) 2021, JFXcore. All rights reserved.
 // Use of this source code is governed by the BSD-3-Clause license that can be found in the LICENSE file.
 
 package org.jfxcore.compiler;
@@ -10,22 +10,10 @@ import java.util.Collections;
 
 public class TestCompilationContext extends CompilationContext {
 
-    private final Logger logger = new Logger() {
-        @Override public void fine(String message) {}
-        @Override public void info(String message) {}
-        @Override public void warning(String message) {}
-        @Override public void error(String message) {}
-    };
-
     public TestCompilationContext(ClassPool classPool) {
         super(new CompilationSource.InMemory(""));
         setClassPool(classPool);
         setImports(Collections.emptyList());
-    }
-
-    @Override
-    public Logger getLogger() {
-        return logger;
     }
 
 }
