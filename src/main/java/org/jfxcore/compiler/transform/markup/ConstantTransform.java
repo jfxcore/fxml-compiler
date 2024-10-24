cc32eaf84a1366e6c125d8dd3e6945c001588bd5
mstr2 <43553916+mstr2@users.noreply.github.com>
2024-10-24 03:19:11 +0200

    fix error message

--- src/main/java/org/jfxcore/compiler/transform/markup/ConstantTransform.java
+++ src/main/java/org/jfxcore/compiler/transform/markup/ConstantTransform.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2023, JFXcore. All rights reserved.
+// Copyright (c) 2023, 2024, JFXcore. All rights reserved.
 // Use of this source code is governed by the BSD-3-Clause license that can be found in the LICENSE file.
 
 package org.jfxcore.compiler.transform.markup;
