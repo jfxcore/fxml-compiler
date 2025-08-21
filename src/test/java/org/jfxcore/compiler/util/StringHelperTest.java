a60bb0393ef050677de7dbd22cbb5370f117b1a2
mstr2 <43553916+mstr2@users.noreply.github.com>
2025-08-21 20:40:47 +0200

    Unescape backslashes and dollar signs correctly

--- src/test/java/org/jfxcore/compiler/util/StringHelperTest.java
+++ src/test/java/org/jfxcore/compiler/util/StringHelperTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, JFXcore. All rights reserved.
+// Copyright (c) 2021, 2025, JFXcore. All rights reserved.
 // Use of this source code is governed by the BSD-3-Clause license that can be found in the LICENSE file.
 
 package org.jfxcore.compiler.util;
@@ -35,4 +35,9 @@ public class StringHelperTest {
         assertEquals("'\"✖\"'", StringHelper.quote("\"✖\""));
     }
 
+    @Test
+    public void Unescape_Xml_String() {
+        String result = StringHelper.unescapeXml("  &lt;foo &#123;&#36;&#125;bar  &gt;");
+        assertEquals("  <foo {$}bar  >", result);
+    }
 }
