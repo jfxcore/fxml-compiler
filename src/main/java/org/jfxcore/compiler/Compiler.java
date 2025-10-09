e1e99d987f92a044ca42f4da60365e069f96a668
mstr2 <43553916+mstr2@users.noreply.github.com>
2025-10-09 15:21:03 +0200

    Fix another class not found error when markup library is not on the classpath

--- src/main/java/org/jfxcore/compiler/Compiler.java
+++ src/main/java/org/jfxcore/compiler/Compiler.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, 2023, JFXcore. All rights reserved.
+// Copyright (c) 2022, 2025, JFXcore. All rights reserved.
 // Use of this source code is governed by the BSD-3-Clause license that can be found in the LICENSE file.
 
 package org.jfxcore.compiler;
@@ -328,7 +328,7 @@ public class Compiler extends AbstractCompiler {
             try {
                 classPool.appendClassPath(path.getAbsolutePath());
             } catch (NotFoundException ex) {
-                throw new RuntimeException("Search path dependency not found: " + ex.getMessage());
+                throw GeneralErrors.internalError("Search path dependency not found: " + ex.getMessage());
             }
         }
 
@@ -341,5 +341,4 @@ public class Compiler extends AbstractCompiler {
             return new Compilation(document, rootClass, context);
         }
     }
-
 }
