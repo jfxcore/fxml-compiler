49cfd34b72ca4c2d08ab6e6697a01459bd2787b4
mstr2 <43553916+mstr2@users.noreply.github.com>
2026-03-19 18:28:12 +0100

    Upgrade shadow plugin to 9.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     `maven-publish`
     signing
     kotlin("jvm") version "2.3.10"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.4.0"
 }
 
 group = "org.jfxcore"
