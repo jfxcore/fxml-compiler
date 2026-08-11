d3b25f4e9e7eedc8d4050aca7b796030cc7442c9
mstr2 <43553916+mstr2@users.noreply.github.com>
2026-08-11 02:04:22 +0200

    Convert tabs to spaces (#261)

--- src/main/java/org/jfxcore/compiler/ClassGenerator.java
+++ src/main/java/org/jfxcore/compiler/ClassGenerator.java
@@ -33,170 +33,170 @@ import java.util.Set;
 @SuppressWarnings("unused")
 public final class ClassGenerator extends AbstractCompiler {
 
-	private final Logger logger;
-	private final Map<Path, CompilationUnitInfo> compilationUnits = new HashMap<>();
+    private final Logger logger;
+    private final Map<Path, CompilationUnitInfo> compilationUnits = new HashMap<>();
 
-	public ClassGenerator(Set<Path> searchPath, Logger logger) {
-		super(searchPath);
-		this.logger = Objects.requireNonNull(logger, "logger");
-	}
+    public ClassGenerator(Set<Path> searchPath, Logger logger) {
+        super(searchPath);
+        this.logger = Objects.requireNonNull(logger, "logger");
+    }
 
-	/**
-	 * Adds an FXML markup file to the current generator and returns whether the file will be compiled.
-	 *
-	 * @param sourceRoot the base source directory (the root of the package namespace)
-	 * @param sourceFile the FXML markup file, either as an absolute path or relative to {@code sourceRoot}
-	 * @return {@code true} if the file will be compiled, {@code false} otherwise
-	 * @throws IOException if an I/O error occurs
-	 * @throws MarkupException if a markup error occurs
-	 */
-	public boolean addFileSource(Path sourceRoot, Path sourceFile) throws IOException {
-		String sourceText = Files.readString(sourceRoot.resolve(sourceFile));
-		return addSource(sourceRoot, sourceFile, sourceText, null);
-	}
+    /**
+     * Adds an FXML markup file to the current generator and returns whether the file will be compiled.
+     *
+     * @param sourceRoot the base source directory (the root of the package namespace)
+     * @param sourceFile the FXML markup file, either as an absolute path or relative to {@code sourceRoot}
+     * @return {@code true} if the file will be compiled, {@code false} otherwise
+     * @throws IOException if an I/O error occurs
+     * @throws MarkupException if a markup error occurs
+     */
+    public boolean addFileSource(Path sourceRoot, Path sourceFile) throws IOException {
+        String sourceText = Files.readString(sourceRoot.resolve(sourceFile));
+        return addSource(sourceRoot, sourceFile, sourceText, null);
+    }
 
-	/**
-	 * Adds FXML markup text to the current generator.
-	 *
-	 * @param sourceRoot the base source directory (the root of the package namespace)
-	 * @param sourceFile the source file path, either as an absolute path or relative to {@code sourceRoot}
-	 * @param sourceText the FXML markup text
-	 * @param additionalImports additional imports from the embedding environment
-	 * @param embeddingHost the name of the embedding host class
-	 * @param sourceOffset the source offset of the embedded markup in the embedding file
-	 * @throws MarkupException if a markup error occurs
-	 */
-	public void addEmbeddedSource(Path sourceRoot,
-								  Path sourceFile,
-								  String sourceText,
-								  List<String> additionalImports,
-								  QualifiedName embeddingHost,
-								  Location sourceOffset) {
-		var embeddingContext = new EmbeddingContext(
-			Objects.requireNonNull(additionalImports, "additionalImports"),
-			Objects.requireNonNull(embeddingHost, "embeddingHost"),
-			Objects.requireNonNull(sourceOffset, "sourceOffset"));
+    /**
+     * Adds FXML markup text to the current generator.
+     *
+     * @param sourceRoot the base source directory (the root of the package namespace)
+     * @param sourceFile the source file path, either as an absolute path or relative to {@code sourceRoot}
+     * @param sourceText the FXML markup text
+     * @param additionalImports additional imports from the embedding environment
+     * @param embeddingHost the name of the embedding host class
+     * @param sourceOffset the source offset of the embedded markup in the embedding file
+     * @throws MarkupException if a markup error occurs
+     */
+    public void addEmbeddedSource(Path sourceRoot,
+                                  Path sourceFile,
+                                  String sourceText,
+                                  List<String> additionalImports,
+                                  QualifiedName embeddingHost,
+                                  Location sourceOffset) {
+        var embeddingContext = new EmbeddingContext(
+            Objects.requireNonNull(additionalImports, "additionalImports"),
+            Objects.requireNonNull(embeddingHost, "embeddingHost"),
+            Objects.requireNonNull(sourceOffset, "sourceOffset"));
 
-		if (!addSource(sourceRoot, sourceFile, sourceText, embeddingContext)) {
-			throw new IllegalArgumentException("The specified markup text will not be compiled");
-		}
-	}
+        if (!addSource(sourceRoot, sourceFile, sourceText, embeddingContext)) {
+            throw new IllegalArgumentException("The specified markup text will not be compiled");
+        }
+    }
 
-	private boolean addSource(Path sourceRoot, Path sourceFile, String sourceText, EmbeddingContext embeddingContext) {
-		sourceRoot = Objects.requireNonNull(sourceRoot, "sourceRoot").normalize();
-		sourceFile = Objects.requireNonNull(sourceFile, "sourceFile").normalize();
-		sourceText = Objects.requireNonNull(sourceText, "sourceText");
+    private boolean addSource(Path sourceRoot, Path sourceFile, String sourceText, EmbeddingContext embeddingContext) {
+        sourceRoot = Objects.requireNonNull(sourceRoot, "sourceRoot").normalize();
+        sourceFile = Objects.requireNonNull(sourceFile, "sourceFile").normalize();
+        sourceText = Objects.requireNonNull(sourceText, "sourceText");
 
-		if (!sourceRoot.isAbsolute()) {
-			throw new IllegalArgumentException("The source directory must be specified as an absolute path");
-		}
+        if (!sourceRoot.isAbsolute()) {
+            throw new IllegalArgumentException("The source directory must be specified as an absolute path");
+        }
 
-		if (sourceFile.isAbsolute()) {
-			try {
-				sourceFile = sourceRoot.relativize(sourceFile);
-			} catch (IllegalArgumentException ex) {
-				throw new IllegalArgumentException("The specified file is not in the source directory");
-			}
-		}
+        if (sourceFile.isAbsolute()) {
+            try {
+                sourceFile = sourceRoot.relativize(sourceFile);
+            } catch (IllegalArgumentException ex) {
+                throw new IllegalArgumentException("The specified file is not in the source directory");
+            }
+        }
 
-		Path absoluteSourceFile = sourceRoot.resolve(sourceFile);
+        Path absoluteSourceFile = sourceRoot.resolve(sourceFile);
 
-		if (!absoluteSourceFile.startsWith(sourceRoot)) {
-			throw new IllegalArgumentException("The specified file is not in the source directory");
-		}
+        if (!absoluteSourceFile.startsWith(sourceRoot)) {
+            throw new IllegalArgumentException("The specified file is not in the source directory");
+        }
 
-		if (compilationUnits.containsKey(absoluteSourceFile)) {
-			throw new IllegalArgumentException("The specified file was already added");
-		}
+        if (compilationUnits.containsKey(absoluteSourceFile)) {
+            throw new IllegalArgumentException("The specified file was already added");
+        }
 
-		try {
-			CompilationUnitInfo compilationUnit = parseSingleFile(sourceRoot, sourceFile, sourceText, embeddingContext);
-			compilationUnits.put(absoluteSourceFile, compilationUnit);
-			return true;
-		} catch (FxmlParseAbortException ex) {
-			logger.info(String.format("File skipped: %s (%s)", absoluteSourceFile, ex.getMessage()));
-			return false;
-		} catch (MarkupException ex) {
-			ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : new Location(0, 0));
-			ex.setSourceFile(absoluteSourceFile.toFile());
-			throw ex;
-		}
-	}
+        try {
+            CompilationUnitInfo compilationUnit = parseSingleFile(sourceRoot, sourceFile, sourceText, embeddingContext);
+            compilationUnits.put(absoluteSourceFile, compilationUnit);
+            return true;
+        } catch (FxmlParseAbortException ex) {
+            logger.info(String.format("File skipped: %s (%s)", absoluteSourceFile, ex.getMessage()));
+            return false;
+        } catch (MarkupException ex) {
+            ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : new Location(0, 0));
+            ex.setSourceFile(absoluteSourceFile.toFile());
+            throw ex;
+        }
+    }
 
-	/**
-	 * Generates Java stub files for all FXML markup files that were added to this generator.
-	 *
-	 * @throws MarkupException if a markup error occurs
-	 */
-	public List<CompilationUnit> process() {
-		if (compilationUnits.isEmpty()) {
-			return List.of();
-		}
+    /**
+     * Generates Java stub files for all FXML markup files that were added to this generator.
+     *
+     * @throws MarkupException if a markup error occurs
+     */
+    public List<CompilationUnit> process() {
+        if (compilationUnits.isEmpty()) {
+            return List.of();
+        }
 
-		try {
-			var transformer = Transformer.getCodeTransformer(newClassPool());
-			var result = new ArrayList<CompilationUnit>();
+        try {
+            var transformer = Transformer.getCodeTransformer(newClassPool());
+            var result = new ArrayList<CompilationUnit>();
 
-			for (var entry : compilationUnits.entrySet()) {
-				CompilationUnitDescriptor descriptor = entry.getValue().descriptor();
-				var context = new CompilationContext(new CompilationSource.InMemory(descriptor.sourceText()));
+            for (var entry : compilationUnits.entrySet()) {
+                CompilationUnitDescriptor descriptor = entry.getValue().descriptor();
+                var context = new CompilationContext(new CompilationSource.InMemory(descriptor.sourceText()));
 
-				try (var ignored = new CompilationScope(context)) {
-					ClassInfo classInfo = processSingleFile(entry.getValue(), transformer);
-					result.add(new CompilationUnit(descriptor, classInfo.sourceText()));
-				} catch (MarkupException ex) {
-					ex.setSourceFile(descriptor.absoluteSourceFile().toFile());
-					throw ex;
-				}
-			}
+                try (var ignored = new CompilationScope(context)) {
+                    ClassInfo classInfo = processSingleFile(entry.getValue(), transformer);
+                    result.add(new CompilationUnit(descriptor, classInfo.sourceText()));
+                } catch (MarkupException ex) {
+                    ex.setSourceFile(descriptor.absoluteSourceFile().toFile());
+                    throw ex;
+                }
+            }
 
-			return List.copyOf(result);
-		} finally {
-			compilationUnits.clear();
-		}
-	}
+            return List.copyOf(result);
+        } finally {
+            compilationUnits.clear();
+        }
+    }
 
-	private CompilationUnitInfo parseSingleFile(Path sourceRoot,
-												Path sourceFile,
-												String sourceText,
-												EmbeddingContext embeddingContext) {
-		CompilationContext context = new CompilationContext(new CompilationSource.InMemory(sourceText));
+    private CompilationUnitInfo parseSingleFile(Path sourceRoot,
+                                                Path sourceFile,
+                                                String sourceText,
+                                                EmbeddingContext embeddingContext) {
+        CompilationContext context = new CompilationContext(new CompilationSource.InMemory(sourceText));
 
-		try (var ignored = new CompilationScope(context)) {
-			logger.fine("Parsing FXML markup file " + sourceRoot.resolve(sourceFile));
+        try (var ignored = new CompilationScope(context)) {
+            logger.fine("Parsing FXML markup file " + sourceRoot.resolve(sourceFile));
 
-			var document = new FxmlParser(sourceFile, sourceText, embeddingContext).parseDocument();
-			var markupBaseClass = QualifiedName.ofPath(FileUtil.getMarkupJavaFile(document));
-			var descriptor = new CompilationUnitDescriptor(
-				embeddingContext, markupBaseClass, sourceRoot, sourceFile, sourceText);
+            var document = new FxmlParser(sourceFile, sourceText, embeddingContext).parseDocument();
+            var markupBaseClass = QualifiedName.ofPath(FileUtil.getMarkupJavaFile(document));
+            var descriptor = new CompilationUnitDescriptor(
+                embeddingContext, markupBaseClass, sourceRoot, sourceFile, sourceText);
 
-			return new CompilationUnitInfo(descriptor, document);
-		} catch (MarkupException ex) {
-			ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : new Location(0, 0));
-			ex.setSourceFile(sourceRoot.resolve(sourceFile).normalize().toFile());
-			throw ex;
-		}
-	}
+            return new CompilationUnitInfo(descriptor, document);
+        } catch (MarkupException ex) {
+            ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : new Location(0, 0));
+            ex.setSourceFile(sourceRoot.resolve(sourceFile).normalize().toFile());
+            throw ex;
+        }
+    }
 
-	private ClassInfo processSingleFile(CompilationUnitInfo compilationUnit, Transformer transformer) {
-		DocumentNode document = compilationUnit.document();
-		StringBuilder stringBuilder = new StringBuilder();
-		JavaEmitContext context = new JavaEmitContext(stringBuilder);
+    private ClassInfo processSingleFile(CompilationUnitInfo compilationUnit, Transformer transformer) {
+        DocumentNode document = compilationUnit.document();
+        StringBuilder stringBuilder = new StringBuilder();
+        JavaEmitContext context = new JavaEmitContext(stringBuilder);
 
-		try {
-			document = (DocumentNode)transformer.transform(document, null, null);
-			context.emit(document);
-		} catch (MarkupException ex) {
-			EmbeddingContext embeddingContext = compilationUnit.descriptor().embeddingContext();
-			ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : new Location(0, 0));
-			ex.setSourceFile(compilationUnit.descriptor().absoluteSourceFile().toFile());
-			throw ex;
-		}
+        try {
+            document = (DocumentNode)transformer.transform(document, null, null);
+            context.emit(document);
+        } catch (MarkupException ex) {
+            EmbeddingContext embeddingContext = compilationUnit.descriptor().embeddingContext();
+            ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : new Location(0, 0));
+            ex.setSourceFile(compilationUnit.descriptor().absoluteSourceFile().toFile());
+            throw ex;
+        }
 
-		return new ClassInfo(compilationUnit.descriptor().markupClass(), stringBuilder.toString());
-	}
+        return new ClassInfo(compilationUnit.descriptor().markupClass(), stringBuilder.toString());
+    }
 
-	private record ClassInfo(QualifiedName markupBaseClass, String sourceText) {}
+    private record ClassInfo(QualifiedName markupBaseClass, String sourceText) {}
 
-	private record CompilationUnitInfo(CompilationUnitDescriptor descriptor, DocumentNode document) {}
+    private record CompilationUnitInfo(CompilationUnitDescriptor descriptor, DocumentNode document) {}
 }
--- src/main/java/org/jfxcore/compiler/MarkupCompiler.java
+++ src/main/java/org/jfxcore/compiler/MarkupCompiler.java
@@ -39,121 +39,121 @@ import java.util.Set;
 @SuppressWarnings("unused")
 public final class MarkupCompiler extends AbstractCompiler {
 
-	private final Logger logger;
+    private final Logger logger;
 
-	public MarkupCompiler(Set<Path> searchPath, Logger logger) {
-		super(searchPath);
-		this.logger = Objects.requireNonNull(logger, "logger");
-	}
+    public MarkupCompiler(Set<Path> searchPath, Logger logger) {
+        super(searchPath);
+        this.logger = Objects.requireNonNull(logger, "logger");
+    }
 
-	/**
-	 * Returns whether the specified class file was compiled by this compiler.
-	 *
-	 * @param classFile the FXML class file
-	 * @return {@code true} if the FXML class file was compiled by this compiler, {@code false} otherwise
-	 * @throws IOException if an I/O error occurs
-	 */
-	public boolean isCompiledFile(Path classFile) throws IOException {
-		return FileUtil.hasGeneratorAttribute(classFile);
-	}
+    /**
+     * Returns whether the specified class file was compiled by this compiler.
+     *
+     * @param classFile the FXML class file
+     * @return {@code true} if the FXML class file was compiled by this compiler, {@code false} otherwise
+     * @throws IOException if an I/O error occurs
+     */
+    public boolean isCompiledFile(Path classFile) throws IOException {
+        return FileUtil.hasGeneratorAttribute(classFile);
+    }
 
-	/**
-	 * Compiles the content of the FXML compilation units.
-	 *
-	 * @throws IOException if an I/O error occurs
-	 * @throws MarkupException if a markup error occurs
-	 */
-	public void compile(Set<CompilationUnitDescriptor> descriptors) throws IOException {
-		if (descriptors.isEmpty()) {
-			return;
-		}
+    /**
+     * Compiles the content of the FXML compilation units.
+     *
+     * @throws IOException if an I/O error occurs
+     * @throws MarkupException if a markup error occurs
+     */
+    public void compile(Set<CompilationUnitDescriptor> descriptors) throws IOException {
+        if (descriptors.isEmpty()) {
+            return;
+        }
 
-		var classPool = newClassPool();
-		var codeTransformer = Transformer.getCodeTransformer(classPool);
-		var bytecodeTransformer = Transformer.getBytecodeTransformer(classPool);
+        var classPool = newClassPool();
+        var codeTransformer = Transformer.getCodeTransformer(classPool);
+        var bytecodeTransformer = Transformer.getBytecodeTransformer(classPool);
 
-		for (CompilationUnitDescriptor descriptor : descriptors) {
-			compileSingleFile(descriptor, codeTransformer, bytecodeTransformer);
-		}
-	}
+        for (CompilationUnitDescriptor descriptor : descriptors) {
+            compileSingleFile(descriptor, codeTransformer, bytecodeTransformer);
+        }
+    }
 
-	private void compileSingleFile(CompilationUnitDescriptor descriptor,
-								   Transformer codeTransformer,
-								   Transformer bytecodeTransformer) throws IOException {
-		CompilationContext context = new CompilationContext(new CompilationSource.InMemory(descriptor.sourceText()));
+    private void compileSingleFile(CompilationUnitDescriptor descriptor,
+                                   Transformer codeTransformer,
+                                   Transformer bytecodeTransformer) throws IOException {
+        CompilationContext context = new CompilationContext(new CompilationSource.InMemory(descriptor.sourceText()));
 
-		try (var ignored = new CompilationScope(context)) {
-			var parser = new FxmlParser(descriptor.sourceFile(), descriptor.sourceText(), descriptor.embeddingContext());
-			var document = parser.parseDocument();
-			var codeDocument = (DocumentNode)codeTransformer.transform(document, null, null);
-			ClassNode classNode = (ClassNode)codeDocument.getRoot();
+        try (var ignored = new CompilationScope(context)) {
+            var parser = new FxmlParser(descriptor.sourceFile(), descriptor.sourceText(), descriptor.embeddingContext());
+            var document = parser.parseDocument();
+            var codeDocument = (DocumentNode)codeTransformer.transform(document, null, null);
+            ClassNode classNode = (ClassNode)codeDocument.getRoot();
 
-			boolean hasCodeBehind = classNode.hasCodeBehind();
-			String packageName = classNode.getPackageName();
-			String codeBehindClassName = packageName + "." + classNode.getClassName();
-			String simpleMarkupClassName = hasCodeBehind ? classNode.getMarkupClassName() : classNode.getClassName();
-			String markupClassName = packageName + "." + simpleMarkupClassName;
-			logger.fine(String.format("Compiling FXML class '%s'", markupClassName));
+            boolean hasCodeBehind = classNode.hasCodeBehind();
+            String packageName = classNode.getPackageName();
+            String codeBehindClassName = packageName + "." + classNode.getClassName();
+            String simpleMarkupClassName = hasCodeBehind ? classNode.getMarkupClassName() : classNode.getClassName();
+            String markupClassName = packageName + "." + simpleMarkupClassName;
+            logger.fine(String.format("Compiling FXML class '%s'", markupClassName));
 
-			URL classUrl = bytecodeTransformer.getClassPool().find(markupClassName);
-			if (classUrl == null) {
-				throw GeneralErrors.internalError(String.format("%s cannot be found", markupClassName));
-			}
+            URL classUrl = bytecodeTransformer.getClassPool().find(markupClassName);
+            if (classUrl == null) {
+                throw GeneralErrors.internalError(String.format("%s cannot be found", markupClassName));
+            }
 
-			var codeBehindClass = TypeDeclaration.of(bytecodeTransformer.getClassPool().get(codeBehindClassName));
-			var markupClass = TypeDeclaration.of(bytecodeTransformer.getClassPool().get(markupClassName));
+            var codeBehindClass = TypeDeclaration.of(bytecodeTransformer.getClassPool().get(codeBehindClassName));
+            var markupClass = TypeDeclaration.of(bytecodeTransformer.getClassPool().get(markupClassName));
 
-			if (markupClass.jvmType().getAttribute(FileUtil.GENERATOR_NAME) != null) {
-				throw GeneralErrors.internalError(String.format(
-					"FXML class '%s' has already been compiled, it cannot be compiled again", markupClassName));
-			}
+            if (markupClass.jvmType().getAttribute(FileUtil.GENERATOR_NAME) != null) {
+                throw GeneralErrors.internalError(String.format(
+                    "FXML class '%s' has already been compiled, it cannot be compiled again", markupClassName));
+            }
 
-			markupClass.jvmType().defrost();
+            markupClass.jvmType().defrost();
 
-			Bytecode bytecode = new Bytecode(markupClass, 1);
+            Bytecode bytecode = new Bytecode(markupClass, 1);
 
-			EmitInitializeRootNode rootNode = (EmitInitializeRootNode)bytecodeTransformer.transform(
-				document, codeBehindClass, markupClass);
+            EmitInitializeRootNode rootNode = (EmitInitializeRootNode)bytecodeTransformer.transform(
+                document, codeBehindClass, markupClass);
 
-			BytecodeEmitContext emitContext = new BytecodeEmitContext(
-				codeBehindClass, markupClass, rootNode, document.getImports(), bytecode);
+            BytecodeEmitContext emitContext = new BytecodeEmitContext(
+                codeBehindClass, markupClass, rootNode, document.getImports(), bytecode);
 
-			emitContext.emitRootNode();
+            emitContext.emitRootNode();
 
-			MethodInfo methodInfo = markupClass.jvmType().getClassFile().getMethod("initializeComponent");
-			if (methodInfo == null) {
-				throw GeneralErrors.internalError("Invalid markup class file");
-			}
+            MethodInfo methodInfo = markupClass.jvmType().getClassFile().getMethod("initializeComponent");
+            if (methodInfo == null) {
+                throw GeneralErrors.internalError("Invalid markup class file");
+            }
 
-			// Add the generator attribute, which is used to detect if a class file was compiled by this compiler.
-			markupClass.jvmType().getClassFile().addAttribute(new AttributeInfo(
-				markupClass.jvmType().getClassFile().getConstPool(), FileUtil.GENERATOR_NAME, new byte[0]));
+            // Add the generator attribute, which is used to detect if a class file was compiled by this compiler.
+            markupClass.jvmType().getClassFile().addAttribute(new AttributeInfo(
+                markupClass.jvmType().getClassFile().getConstPool(), FileUtil.GENERATOR_NAME, new byte[0]));
 
-			methodInfo.setCodeAttribute(bytecode.toCodeAttribute());
-			methodInfo.rebuildStackMap(markupClass.jvmType().getClassPool());
+            methodInfo.setCodeAttribute(bytecode.toCodeAttribute());
+            methodInfo.rebuildStackMap(markupClass.jvmType().getClassPool());
 
-			Path outDir = Paths.get(classUrl.toURI());
-			context.addModifiedClass(markupClass, outDir);
-			emitContext.getNestedClasses().forEach(c -> context.addModifiedClass(c, outDir));
+            Path outDir = Paths.get(classUrl.toURI());
+            context.addModifiedClass(markupClass, outDir);
+            emitContext.getNestedClasses().forEach(c -> context.addModifiedClass(c, outDir));
 
-			flushModifiedClasses(context);
-		} catch (MarkupException ex) {
-			EmbeddingContext embeddingContext = descriptor.embeddingContext();
-			ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : null);
-			ex.setSourceFile(descriptor.absoluteSourceFile().toFile());
-			throw ex;
-		} catch (BadBytecode | URISyntaxException | CannotCompileException ex) {
-			MarkupException m = GeneralErrors.internalError(ex.getMessage());
-			EmbeddingContext embeddingContext = descriptor.embeddingContext();
-			m.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : null);
-			m.setSourceFile(descriptor.absoluteSourceFile().toFile());
-			throw m;
-		} catch (NotFoundException ex) {
-			MarkupException m = SymbolResolutionErrors.classNotFound(SourceInfo.none(), ex.getMessage());
-			EmbeddingContext embeddingContext = descriptor.embeddingContext();
-			m.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : null);
-			m.setSourceFile(descriptor.absoluteSourceFile().toFile());
-			throw m;
-		}
-	}
+            flushModifiedClasses(context);
+        } catch (MarkupException ex) {
+            EmbeddingContext embeddingContext = descriptor.embeddingContext();
+            ex.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : null);
+            ex.setSourceFile(descriptor.absoluteSourceFile().toFile());
+            throw ex;
+        } catch (BadBytecode | URISyntaxException | CannotCompileException ex) {
+            MarkupException m = GeneralErrors.internalError(ex.getMessage());
+            EmbeddingContext embeddingContext = descriptor.embeddingContext();
+            m.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : null);
+            m.setSourceFile(descriptor.absoluteSourceFile().toFile());
+            throw m;
+        } catch (NotFoundException ex) {
+            MarkupException m = SymbolResolutionErrors.classNotFound(SourceInfo.none(), ex.getMessage());
+            EmbeddingContext embeddingContext = descriptor.embeddingContext();
+            m.setSourceOffset(embeddingContext != null ? embeddingContext.sourceOffset() : null);
+            m.setSourceFile(descriptor.absoluteSourceFile().toFile());
+            throw m;
+        }
+    }
 }
